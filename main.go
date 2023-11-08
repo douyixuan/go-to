7bc709160822c05e732f0e3f5bba621370c6ed97
cedric <cedric.meng@outloook.com>
2023-11-08 23:00:06 +0800

    Add a minimal crawler.
    Get the homepage from thepaper.cn and print to the screen.

--- main.go
+++ main.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"io/ioutil"
 )
 
 func main() {
-	fmt.Println("hi")
+	url := "https://thepaper.cn"
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Get url error", err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Printf("error status code: %v", resp.StatusCode)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("read content failed: %v", err)
+	}
+
+	fmt.Println("body:", string(body))
 }
